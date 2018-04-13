fb92b5ac4419ab67ff615e2ba53a659b173f4bd2
MiroBeh <miro.behninger@gmail.com>
2018-04-13 16:40:32 +0200

    5.2 added a search form for the nav & added bootstraps classes for the forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  def self.search(search_term)
+    if :development
+      @Products = Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      @Products = Product.where("name ilike ?", "%#{search_term}%")
+    end
+  end
 end
