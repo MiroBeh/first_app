f21c95b10af4117a9a30ef7aebf7439173b49616
MiroBeh <miro.behninger@gmail.com>
2018-04-16 10:58:21 +0200

    5.2 fixed landingpage row width, test case search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   def self.search(search_term)
     if :development
-      @Products = Product.where("name LIKE ?", "%#{search_term}%")
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
     else
-      @Products = Product.where("name ilike ?", "%#{search_term}%")
+      @products = Product.where("name ILIKE ?", "#{search_term}%")
     end
   end
 end
