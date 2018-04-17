d6ba0bee8dbdd4f95713751c1d7c9d6b6c9b34b9
MiroBeh <miro.behninger@gmail.com>
2018-04-17 07:26:35 +0200

    5.2 added rails env function for right database methode

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    if :development
+    if Rails.env.development?
       @products = Product.where("name LIKE ?", "%#{search_term}%")
     else
       @products = Product.where("name ILIKE ?", "#{search_term}%")
