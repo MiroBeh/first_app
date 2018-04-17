23267b538d5fbfbde2bebf14998509efc6942559
MiroBeh <miro.behninger@gmail.com>
2018-04-17 07:31:12 +0200

    5.2 added env = production

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
   def self.search(search_term)
-    if Rails.env.development?
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
-    else
+    if Rails.env == "production"
       @products = Product.where("name ILIKE ?", "#{search_term}%")
+    else
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
     end
   end
 end
