de6c2699ae2c785770e183dcd96705e7ada19363
MiroBeh <miro.behninger@gmail.com>
2018-05-07 16:07:27 +0200

    5.10 add admin flagand function to delete comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user
-    can :manage, User, id: user.id
+    can :create, :read, :update, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    end
   end
 end
